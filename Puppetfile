7751abfb0e56e7059bb77a7e5ac00c91df536157
Karl M. Davis <karl@justdavis.com>
2013-11-02 20:29:01 -0700

    Added Sonar to eddings.
    Also added PostgreSQL, to store the Sonar DB.

--- Puppetfile
+++ Puppetfile
@@ -8,3 +8,10 @@ forge "http://forge.puppetlabs.com"
 # mod 'apt',
 #   :git => 'https://github.com/puppetlabs/puppetlabs-apt.git',
 #   :ref => 'feature/master/dans_refactor'
+
+# Can be used to install and configure PostgreSQL and DBs for it.
+mod 'puppetlabs/postgresql'
+
+# Can be used to install and configure Sonar.
+mod 'sonar',
+   :git => 'https://github.com/karlmdavis/puppet-sonar'
---
+++ manifests/nodes/eddings.pp
@@ -0,0 +1,36 @@
+# The configuration file for this node.
+node 'eddings.justdavis.com' {
+  # Install PostgreSQL.
+  class { 'postgresql::server':
+    listen_addresses => 'localhost',
+  }
+
+  # Add a PostgreSQL DB for Sonar to use.
+  $sonar_db_password='sonarpassword'
+  postgresql::server::db { 'sonar':
+    user     => 'sonar',
+    password => postgresql_password('sonar', $sonar_db_password),
+    encoding => 'UTF8',
+  }
+
+  # Install Sonar.
+  $sonar_jdbc={
+    url               => 'jdbc:postgresql://localhost/sonar',
+    username          => 'sonar',
+    password          => $sonar_db_password,
+  }
+  $sonar_ldap={
+    url           => 'ldaps://ldap.justdavis.com',
+    user_base_dn  => 'ou=people,dc=justdavis,dc=com',
+    group_base_dn => 'ou=groups,dc=justdavis,dc=com',
+  }
+  class { 'maven::maven' : } ->
+  class { 'sonar' :
+    version      => '3.7.3',
+    port         => '9000',
+    context_path => '/sonar',
+    jdbc         => $sonar_jdbc,
+    ldap         => $sonar_ldap,
+    require      => Postgresql::Server::Db['sonar'],
+  }
+}
